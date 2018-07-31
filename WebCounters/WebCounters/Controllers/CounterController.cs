2363fdfe6cef9a63573f6eb52a1d5d600b310c2b
Rafał Próchnicki <rafal@progryf.pl>
2018-07-31 08:09:56 +0200

    vvv

---
+++ WebCounters/WebCounters/Controllers/CounterController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebCounters.Models;
+
+namespace WebCounters.Controllers
+{
+    public class CounterController : Controller
+    {
+
+        private const string KEY = "COUNTER";
+
+        public ActionResult ShowCountersUsingViewBag()
+        {
+
+            ViewBag.SessionCounter = getSessionCounter().NextValue;
+            ViewBag.StaticCounter = getStaticCounter().NextValue;
+
+            return View();
+        }
+
+        private BaseCounter getSessionCounter()
+        {
+            if (Session[KEY] == null)
+                Session[KEY] = new NormalCounter();
+
+            return (BaseCounter)Session[KEY];
+        }
+
+        private BaseCounter getStaticCounter()
+        {
+            return new StaticCounter();
+        }
+    }
+}
--- WebCounters/WebCounters/Models/Counters.cs
+++ WebCounters/WebCounters/Models/Counters.cs
@@ -5,7 +5,35 @@ using System.Web;
 
 namespace WebCounters.Models
 {
-    public class Counters
+    abstract public class BaseCounter
     {
+
+        public int NextValue { get => getNextValue(); }
+
+        protected abstract int getNextValue();
+
+    }
+
+    public class NormalCounter : BaseCounter
+    {
+        //Normalna zmienna - zmienna obiektu
+        private int counter = 0;
+
+        protected override int getNextValue()
+        {
+            return counter++;
+        }
+
+    }
+
+    public class StaticCounter : BaseCounter
+    {
+        //Zmienna statyczna - zmienna klasy
+        private static int counter = 0;
+
+        protected override int getNextValue()
+        {
+            return counter++;
+        }
     }
 }
