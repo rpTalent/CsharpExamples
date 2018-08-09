37ccb8ac2cd8b9d67c8b9f6ec9e7498f1ea459f9
Rafał Próchnicki <rafal@progryf.pl>
2018-08-09 19:36:18 +0200

    Koniec

---
+++ WebModel_002/WebModel_002/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebModel_002.Models;
+
+namespace WebModel_002.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private const string KEY = "PROVIDER";
+
+        public PersonProvider Provider { get { return getProvider(); } }
+
+        public ActionResult Index()
+        {
+            return View(Provider);
+        }
+
+        //Akcja InsertOrUpdate, poprzez wywoalnie POST zapisuje lub dodaje do listy i przekierowuje do akcji Insert, wyswietlajacej liste 
+        [HttpPost]
+        public ActionResult InsertOrUpdate(Person value)
+        {
+            if (!ModelState.IsValid)
+                return View(value);
+            Provider.InsertOrUpdate(value);
+            return RedirectToAction("Index");
+        }
+
+        //Akcja Insert, porzez domyslne wywolanie GET prezkierowuje do widoku InsertOrUpdate, wyswietlajacego jedynie formularz 
+        public ActionResult Insert()
+        {
+            return View("InsertOrUpdate");
+        }
+
+        //Akcja Update, porzez domyslne wywolanie GET prezkierowuje do widoku InsertOrUpdate, do widoku dostarcza obiekt Person
+        public ActionResult Update(string id)
+        {
+            return View("InsertOrUpdate", Provider.Find(id));
+        }
+
+        public ActionResult Delete(string id)
+        {
+            Provider.Remove(id);
+            return RedirectToAction("Index");
+        }
+
+        private PersonProvider getProvider()
+        {
+            if (Session[KEY] == null)
+                Session[KEY] = new PersonProvider();
+            return (PersonProvider)Session[KEY];
+        }
+
+
+    }
+}
---
+++ WebModel_002/WebModel_002/Models/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebModel_002.Models
+{
+    public class Person
+    {
+        public string PersonId { get; set; }
+
+        [Display(Name = "Imię")]
+        [Required(ErrorMessage = "Pole Imię jest wymagane")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Nazwisko")]
+        [Required(ErrorMessage = "Pole Nazwisko jest wymagane")]
+        public string SureName { get; set; }
+
+        [Display(Name = "Płeć")]
+        [Required(ErrorMessage = "Pole Płeć jest wymagane")]
+        public Char Sex { get; set; }
+
+        [Display(Name = "Data urodzenia")]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Adres email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Numer telefonu")]
+        public string PhoneNumber { get; set; }
+    }
+}
